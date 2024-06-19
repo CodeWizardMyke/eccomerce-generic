4cd959255aacc04bdac65e683ed82f6ec9cf5d76
myke william oliveira feitosa <maikewilliam.dev@gmail.com>
2024-06-19 00:38:26 -0300

    FEAT add header in project

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { IoMenu } from "react-icons/io5";
+import { RxGithubLogo } from "react-icons/rx";
+import { BsCart3 } from "react-icons/bs";
+import { CiSearch } from "react-icons/ci";
+
+import './Header.css'
 
 function Header() {
   return (
-    <header>
-        header
+    <header className='conteiner'>
+      <div className='header'>
+      <button type='button' className='btn-menu'><IoMenu /></button>
+      <div className='content-logo'>
+        <RxGithubLogo />
+        <span>E-eccomerce</span>
+      </div>
+      <button type='button' className='btn-cart'><BsCart3 /></button>
+      </div>
+      <div className="search-bar">
+        <input type="search" name="searchBar"  />
+        <button type='submit'><CiSearch /></button>
+      </div>
     </header>
   )
 }
