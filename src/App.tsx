2cf87f23bd31376e980b3ff1c74539b47e929b82
myke william oliveira feitosa <maikewilliam.dev@gmail.com>
2024-06-18 23:58:05 -0300

    FIX removido alguns bugs com npm audt fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <div>
-      <h2>empty</h2>
-    </div>
+    <>
+    <Header/>
+      <Outlet/>
+    <Footer/>
+    </>
   );
-}
+};
 
 export default App;
